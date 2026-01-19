0e3c6873b8a6e5707f56daab5d6e6a12bee0eb2d
Rachelllk97 <rachelkellyprod@gmail.com>
2026-01-19 15:42:58 +0000

    created create_space route on app file

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ app = Flask(__name__)
 def get_index():
     return render_template('index.html')
 
+@app.route('/create_space', methods=['POST'])
+def create_space():
+    return render_template('create_space.html')
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
