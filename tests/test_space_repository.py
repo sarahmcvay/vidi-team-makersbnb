8f007f257ec2083e25bc8a0df88056620ec9b0db
Rachelllk97 <rachelkellyprod@gmail.com>
2026-01-22 13:58:12 +0000

    updated space repository tests to reflect image links

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -7,28 +7,28 @@ def test_get_all_spaces(db_connection):
     repository = SpaceRepository(db_connection)
     spaces = repository.all()
     assert spaces == [
-        Space(1, 'space1', 10.00, 'great house', 'http123', 1),
-        Space(2, 'space2', 20.00, 'ok house', 'http456', 2),
-        Space(3, 'space3', 30.00, 'fun house', 'http789', 3)
+        Space(1, 'space1', 10.00, 'great house', 'https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D', 1),
+        Space(2, 'space2', 20.00, 'ok house', 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG91c2V8ZW58MHx8MHx8fDA%3D', 2),
+        Space(3, 'space3', 30.00, 'fun house', 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8fDA%3D', 3)
     ]
 
 
 def test_create_space(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
     repository = SpaceRepository(db_connection)
-    repository.create(Space(None,'space4', 40.00, 'cool house', 'http668', 4))
+    repository.create(Space(None,'space4', 40.00, 'cool house', 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D', 4))
     result = repository.all()
     assert result == [
-        Space(1, 'space1', 10.00, 'great house', 'http123', 1),
-        Space(2, 'space2', 20.00, 'ok house', 'http456', 2),
-        Space(3, 'space3', 30.00, 'fun house', 'http789', 3),
-        Space(4, 'space4', 40.00, 'cool house', 'http668', 4)
+        Space(1, 'space1', 10.00, 'great house', 'https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D', 1),
+        Space(2, 'space2', 20.00, 'ok house', 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG91c2V8ZW58MHx8MHx8fDA%3D', 2),
+        Space(3, 'space3', 30.00, 'fun house', 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8fDA%3D', 3),
+        Space(4, 'space4', 40.00, 'cool house', 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D', 4)
     ]
 
 def test_get_space_id_by_user_id(db_connection):
     db_connection.seed("seeds/makersbnb.sql")
     repository = SpaceRepository(db_connection)
-    repository.create(Space(None,'space4', 40.00, 'cool house', 'http668', 1))
+    repository.create(Space(None,'space4', 40.00, 'cool house', 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8fDA%3D', 1))
     space_id = repository.get_space_id_by_user_id(1)
     assert space_id == [1, 4]
     
@@ -38,7 +38,7 @@ def test_get_spaces_by_user_id(db_connection):
     user_id = 1 
     result = repository.get_spaces_by_user_id(user_id)
     assert result == [
-        Space(1, 'space1', 10.00, 'great house', 'http123', 1)
+        Space(1, 'space1', 10.00, 'great house', 'https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8fDA%3D', 1)
     ]
 
 
